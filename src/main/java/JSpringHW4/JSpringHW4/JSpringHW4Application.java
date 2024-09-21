20c2cd4a1e69ffdfcb81c44730811e8a8b19c3a2
MonyaDeseptikon <isadom@mail.ru>
2024-09-21 11:37:44 +0300

    HW4

---
+++ src/main/java/JSpringHW4/JSpringHW4/JSpringHW4Application.java
@@ -0,0 +1,13 @@
+package JSpringHW4.JSpringHW4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSpringHW4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSpringHW4Application.class, args);
+	}
+
+}
---
+++ src/test/java/JSpringHW4/JSpringHW4/JSpringHw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package JSpringHW4.JSpringHW4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSpringHw4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
